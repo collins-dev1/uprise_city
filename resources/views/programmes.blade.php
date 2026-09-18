acef1b34e1ea9b46b3d6e62f3943d96b3ddd6739
OG-COLLINS/ <collinsxuccessful39@gmail.com>
2026-09-19 00:12:51 +0100

    smartsupp setup

--- resources/views/programmes.blade.php
+++ resources/views/programmes.blade.php
@@ -9,6 +9,19 @@
         content="Explore Uprise City's certificate programmes — Personal Mastermind, Biz Encyclopedia, and Life Planning & Career Advancement.">
     <link rel="stylesheet" href="{{ asset('uprise-city/css/style.css') }}" />
     <link rel="icon" href="{{ asset('uprise-city/img/uprise.jpg') }}">
+    <!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '0ae0f199cd395e08330a596f081c0052b49b7e2c';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+
 </head>
 
 <body>
